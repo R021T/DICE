8bea81ded6115aa5e0ebdfba4584780287443ddc
Rohit C <124383636+R021T@users.noreply.github.com>
2023-09-10 02:37:09 +0530

    Update views.py

--- P1/app1/views.py
+++ P1/app1/views.py
@@ -3,10 +3,10 @@ import random
 
 # Create your views here.
 def home(request):
-    return render(request,'home.html',{'a':45,'b':45})
+    return render(request,'base.html')
 
 def roll(request):
     lx={1:0,2:-90,3:0,4:0,5:90,6:180}
     ly={1:0,2:0,3:90,4:-90,5:0,6:0}
     n=random.randrange(1,7)
-    return render(request,'home.html',{'x':lx[n],'y':ly[n],'a':lx[n]+360,'b':ly[n]+360})
+    return render(request,'roll.html',{'x':lx[n],'y':ly[n],'a':lx[n]+360,'b':ly[n]+360})
